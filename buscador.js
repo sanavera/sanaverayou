c13e7641ef5ab9b8aeb5d249b4b00c8216cb814a
sanavera <sanavera78@gmail.com>
2025-09-15 19:59:45 -0300

    buscador.js

--- buscador.js
+++ buscador.js
@@ -1,5 +1,5 @@
 // --- Constantes del Scraper (Editables) ---
-const SCRAPER_HOST = "http://186.143.203.244:80";
+const SCRAPER_HOST = "http://191.85.27.198:5000";
 
 // Endpoints del servidor
 const scraperYTM = (q) => `${SCRAPER_HOST}/?ytm=${encodeURIComponent(q)}`;
